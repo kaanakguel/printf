26ed53c12db3ef15874a2f8fcfb5ad980fe55fbe
ferhatsor <abdul.sor@stud.thga.de>
2024-06-10 16:54:34 +0200

    format.c

---
+++ format.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  int i=1;
+  float f=1.11;
+  char c='a';
+  char s[]="Ferhat";
+  printf("int: %i , float: %f , char: %c , String: %s",i,f,c,s);
+  return 0;
+}
