5bc6f63dc9be25748cc8bc74dc0f5783d77daaae
kaanakguel <kaan.akguel@stud.thga.de>
2024-06-10 17:04:13 +0200

    ansi\ color .c

---
+++ ansi\ color .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+\033[35m
+int main(void){
+        printf("\033[36m  *****  \033[0m\n");
+        printf("\033[36m *     * \033[0m\n");
+        printf("\033[36m*  o o  *\033[0m\n");
+        printf("\033[36m*   ^   *\033[0m\n");
+        printf("\033[36m*  '-'  *\033[0m\n");
+        printf("\033[36m *     * \033[0m\n");
+        printf("\033[36m  *****  \033[0m\n");
+	return 0;
+}
